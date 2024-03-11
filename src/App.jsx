cfc939db404805f7270aab7ea079e8c07fe759c1
loukalost <131281040+loukalost@users.noreply.github.com>
2024-03-11 16:37:43 +0100

    next commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import { Button } from '@nextui-org/react'
 import './App.css'
+import TodoList from './components/ToDoList'
+
+const todos = [
+  {
+    title: 'todo n°1',
+    description: 'super description n°1'
+  },
+  {
+    title: 'todo n°2',
+    description: 'super description n°2'
+  },
+  {
+    title: 'todo n°3',
+    description: 'super description n°3'
+  },
+  {
+    title: 'todo n°4',
+    description: 'super description n°4'
+  }
+]
 
 function App () {
   return (
     <>
+      <TodoList
+        todos={todos}
+      />
       <Button
         color='primary'
       >
---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,21 @@
+import Todo from './ToDo'
+
+function TodoList ({ todos }) {
+  return (
+    <div className='flex flex-col gap-4 py-4 px-4'>
+      {
+        todos.map((todo) => {
+          return (
+            <Todo
+              key={todo.title}
+              title={todo.title}
+              description={todo.description}
+            />
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default TodoList
